4074b7592dd84c08637eddfffd07fafe419a6b5a
Anta <felloso666@gmail.com>
2020-10-18 21:22:58 +0300

    Ready program

--- Lab1/Source.cpp
+++ Lab1/Source.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 int main()
 {
-	cout << 0;
+	setlocale(LC_ALL, "ukrainian");
+	long double a, V, S;
+	cout << "Введiть ребро куба:";
+	cin >> a;
+	if (a <= 0) 
+	{
+		cout << "Такий куб не iснує" << "\n" ;
+		system("pause");
+		return 0;
+	}
+	
+	V = a*a*a;
+	S = a * a * 6;
+	
+	
+	cout << "Об'єм куба:" << V << " " << "Площа бiчної поверхнi:" << S << "\n";
+	system("pause");
+	return 0;
 }
